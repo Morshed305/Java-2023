93c7645234f2688e6ea3252ab0a043b983c18793
Moreshed <morshedalam9691616@gmail.com>
2024-04-07 03:50:46 +0600

    NetBens-code push from Vscode _Test

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,15 @@
+
+package javaapplication1;
+
+
+public class JavaApplication1 {
+
+  
+    public static void main(String[] args) {
+      int a=19;
+       int b=90;
+       int X=a+b;
+        System.out.println("it's for NetBeans Code test = "+X);
+    }
+    
+}
